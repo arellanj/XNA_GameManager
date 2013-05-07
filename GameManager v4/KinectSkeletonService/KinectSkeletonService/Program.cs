6e448661daeeb78ebd1e2edf1ca49bea67ad47e7
arellanj <jarel002@ucr.edu>
2013-05-07 00:21:08 -0700

    Added the Coding4Fun libraries along with a test Kinect service class that will share the player's skeleton data over a socket

---
+++ GameManager v4/KinectSkeletonService/KinectSkeletonService/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Coding4Fun.Kinect.KinectService.Common;
+using Coding4Fun.Kinect.KinectService.Listeners;
+using Microsoft.Kinect;
+
+namespace KinectSkeletonService
+{
+    class Program
+    {
+        SkeletonListener skeletonListener;
+        static void Main(string[] args)
+        {
+            int socket;
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Needs a parameter for Streaming Socket");
+                return;
+            }
+            else
+            {
+                socket = Convert.ToInt32(args[1]);
+                if (socket < 0 || socket > 65535)
+                {
+                    Console.WriteLine("Socket must be in range 0 - 65535");
+                    return;
+                }
+            }
+            KinectSensor kinect = KinectSensor.KinectSensors[0];
+
+            kinect.SkeletonStream.Enable();
+            kinect.Start();
+
+            SkeletonListener skeletonListener = new SkeletonListener(kinect, socket);
+            skeletonListener.Start();
+        }
+
+        
+    }
+}
